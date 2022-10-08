5f366bfea4e003b263d9e2d40ba834993bb86c9e
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-08 02:41:07 -0300

    Referenciando os posts do usuário
    Arquivo
    16-mongodb-spring-boot.pdf

--- workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
@@ -41,9 +41,10 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("07/10/2022"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/11/2022"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
-
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
