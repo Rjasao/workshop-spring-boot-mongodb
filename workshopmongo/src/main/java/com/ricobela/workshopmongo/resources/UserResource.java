09069065c9a79affb611c010e06961e80c022a3c
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-08 03:33:31 -0300

    Obtendo um post por id
    Arquivo 26
    16-mongodb-spring-boot,pdf

--- workshopmongo/src/main/java/com/ricobela/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/ricobela/workshopmongo/resources/UserResource.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.ricobela.workshopmongo.domain.Post;
 import com.ricobela.workshopmongo.domain.User;
 import com.ricobela.workshopmongo.dto.UserDTO;
 import com.ricobela.workshopmongo.services.UserService;
@@ -59,20 +58,4 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
-	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
-		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj.getPosts());
-	}
-
 }
-
-
-
-
-
-
-
-
-
