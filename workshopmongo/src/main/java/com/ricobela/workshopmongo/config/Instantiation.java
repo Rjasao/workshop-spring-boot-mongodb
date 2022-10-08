fc3036df50c4949e555b224051ae9c5650e0059e
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-07 21:15:34 -0300

    Checklist:  Em UserService, implementar o método delete  Em UserResource, implementar o método delete Atualização de usuário com PUT ATUALIZAÇÃO Para o projeto ficar compatível com Spring Boot 2.x.x: Classe UserService, troque o código antigo: public User update(User obj) { User newObj = repo.findOne(obj.getId()); updateData(newObj, obj); return repo.save(newObj); } Por este: public User update(User obj) { User newObj = findById(obj.getId()); updateData(newObj, obj); return repo.save(newObj); } Checklist:  Em UserService, implementar os métodos update e updateData  Em UserResource, implementar o método update Criando entity Post com User aninhado
    Arquivo 26
    16-mongodb-spring-boot.pdf

--- workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.ricobela.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.ricobela.workshopmongo.domain.Post;
 import com.ricobela.workshopmongo.domain.User;
+import com.ricobela.workshopmongo.repositories.PostRepository;
 import com.ricobela.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -14,16 +18,29 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("07/10/2022"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/11/2022"), "Bom dia", "Acordei feliz hoje", maria);
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 	}
 }
