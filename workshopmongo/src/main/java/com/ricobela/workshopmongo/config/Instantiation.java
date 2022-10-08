6a3f8817b84f9433c68b5703bdb697921563a18c
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-08 03:58:01 -0300

    Acrescentando comentários aos posts
    Arquivo 26
    16-mongodb-spring.boot.pdf

--- workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/ricobela/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.ricobela.workshopmongo.domain.Post;
 import com.ricobela.workshopmongo.domain.User;
 import com.ricobela.workshopmongo.dto.AuthorDTO;
+import com.ricobela.workshopmongo.dto.CommentDTO;
 import com.ricobela.workshopmongo.repositories.PostRepository;
 import com.ricobela.workshopmongo.repositories.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("07/10/2022"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/11/2022"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2022"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
